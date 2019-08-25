85fbbba70509e5dc6b292ed7dd3aaf642ff3071b
Kyle Chen <kyle.h.chen@gmail.com>
2019-08-25 15:35:48 -0700

    Frontend authentication

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -13,9 +13,14 @@ import AlertTemplate from "react-alert-template-basic";
 import { Provider } from "react-redux";
 import store from "../store";
 
+import { loadUser } from "../actions/auth";
+import PrivateRoute from "./common/PrivateRoute";
+
 import Alerts from "./layout/Alerts";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
+import Login from "./accounts/Login";
+import Register from "./accounts/Register";
 
 // Alert options
 const alertOptions = {
@@ -24,6 +29,10 @@ const alertOptions = {
 };
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
@@ -34,7 +43,9 @@ class App extends Component {
               <Alerts />
               <div className="container">
                 <Switch>
-                  <Route exact path="/" component={Dashboard} />
+                  <PrivateRoute exact path="/" component={Dashboard} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
                 </Switch>
               </div>
             </Fragment>
